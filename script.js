d16e6a2a88c13cbf67ed3938e1ad4dbe75190ed6
SanjayMeena07 <97653788+SanjayMeena07@users.noreply.github.com>
2025-02-09 00:32:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.getElementById("yes-btn");
 const noBtn = document.getElementById("no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "Yay, see you on the 19th of Jan!";
+  question.innerHTML = "Yay, call me!";
   gif.src = "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
 
